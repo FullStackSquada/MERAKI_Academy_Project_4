a2d59615c2f46ec664eef32ec80a388eac7fb2a2
ferasdiab <dfiras96@gmail.com>
2021-08-17 12:38:26 +0300

    fixbuginProfile

--- frontend/src/components/edituserinfo.js
+++ frontend/src/components/edituserinfo.js
@@ -1,20 +1,41 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import axios from "axios";
 import { Switch, Route, Link, useParams, useHistory } from "react-router-dom";
 
-const result = JSON.parse(localStorage.getItem("userInfo"));
+// const result = JSON.parse(localStorage.getItem("userInfo"));
 
 const EditUserInfo = () => {
   const history = useHistory();
-
-  const [lastName, setLastName] = useState(result.lastName);
-  const [firstName, setFirstName] = useState(result.firstName);
-  const [age, setAge] = useState(result.age);
-  const [phoneNumber, setPhoneNumber] = useState(result.phoneNumber);
-  const [gender, setGender] = useState(result.gender);
-  const [country, setCountry] = useState(result.country);
+  const [lastName, setLastName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [age, setAge] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [gender, setGender] = useState("");
+  const [country, setCountry] = useState("");
+  const [email, setEmail] = useState("")
+  const [result, setResult] = useState("")
   let thisToken = localStorage.getItem("token");
-
+  
+  useEffect(() => {
+    axios.get(`http://localhost:5000/user/id`,    {
+      headers: {
+        authorization: "Bearer " + thisToken,
+      },
+    }).then(result=>{
+      setResult(result.data)
+      setLastName(result.data.lastName)
+      setFirstName(result.data.setFirstName)
+      setAge(result.data.setAge)
+      setPhoneNumber(result.data.setPhoneNumber)
+      setGender(result.data.setGender)
+      setCountry(result.data.setCountry)
+      setEmail(result.data.email)
+    }).catch(err=>{
+      console.log(err);
+    })
+    
+    }
+  , [])
   const modifyInfo = () => {
     axios
       .put(
@@ -46,7 +67,8 @@ const EditUserInfo = () => {
 
   return (
     <div className="edit-info">
-      <p id="your-information"> Edit Your Profile</p>
+      {result ? <div>
+        <p id="your-information"> Edit Your Profile</p>
 
       <div className="user-info1">
         <div class="info-s">
@@ -66,38 +88,38 @@ const EditUserInfo = () => {
         </div>
         <div className="input-edit">
           <input
-            placeholder={result.data.firstName}
+            placeholder={result.firstName}
             onChange={(e) => {
               setFirstName(e.target.value);
             }}
           />{" "}
           <input
-            placeholder={result.data.lastName}
+            placeholder={result.lastName}
             onChange={(e) => {
               setLastName(e.target.value);
             }}
           />{" "}
           <input
-            placeholder={result.data.age}
+            placeholder={result.age}
             onChange={(e) => {
               setAge(e.target.value);
             }}
           />{" "}
           <input
-            placeholder={result.data.phoneNumber}
+            placeholder={result.phoneNumber}
             onChange={(e) => {
               setPhoneNumber(e.target.value);
             }}
           />{" "}
-          <input id="email" value={result.data.email} />
+          <input id="email" value={email} />
           <input
-            placeholder={result.data.gender}
+            placeholder={result.gender}
             onChange={(e) => {
               setGender(e.target.value);
             }}
           />{" "}
           <input
-            placeholder={result.data.country}
+            placeholder={result.country}
             onChange={(e) => {
               setCountry(e.target.value);
             }}
@@ -105,7 +127,7 @@ const EditUserInfo = () => {
           <br />
           <button onClick={modifyInfo}>Update</button>
         </div>
-      </div>
+      </div> </div>:"" }
     </div>
   );
 };
