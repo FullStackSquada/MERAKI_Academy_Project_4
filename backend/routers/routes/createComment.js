666e53d36b69d588e67360deb9295204053a64ce
ferasdiab <dfiras96@gmail.com>
2021-08-18 12:26:04 +0300

    add delete comment for back

--- backend/routers/routes/createComment.js
+++ backend/routers/routes/createComment.js
@@ -3,9 +3,10 @@ const express = require('express');
 
 const createNewCommentRouter = express.Router();
 
-const {createComment} = require("../controllers/createComment")
+const {createComment,deleteComment} = require("../controllers/createComment")
 const authentication = require("../../routers/middlewares/authentication")
 
 createNewCommentRouter.post("/products/:id/comments",authentication,createComment);
+createNewCommentRouter.delete("/products/:id/comments/:comment_id",deleteComment)
 
 module.exports = createNewCommentRouter;
